eceefbd8f1e8bc7c98ed98d0dfca9fc13a68833e
Adrian1706 <adrianjbuenop@gmail.com>
2023-09-13 23:35:41 -0500

    feat: :sparkles: Configuracion y activacion de servidores, tanto backend como frontend
    Se crearon ambos servidores y se hicieron pruebas para levantarlos

--- Backend/app.js
+++ Backend/app.js
@@ -1,2 +1,19 @@
 import dotenv from "dotenv";
 import express from "express";
+import versionRoutes from "express-routes-versioning";
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+const config = JSON.parse(process.env.MY_SERVER);
+const versionRoute = versionRoutes();
+
+app.use((req, res, next) => {
+  req.version = req.headers["accept-version"]|| "1.0.0";
+  next();
+});
+
+app.listen(config.port, config.hostname, () => {
+    console.log(`Servidor iniciado en http://${config.hostname}:${config.port}`);
+  });
